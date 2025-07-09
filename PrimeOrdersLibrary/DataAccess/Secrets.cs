9906b0acb59d806e3b63e541ea823e19dfb5b876
Aadi Poddar <aadipoddarmail@gmail.com>
2025-07-09 12:14:48 +0530

    Ignore Secrets

--- PrimeOrdersLibrary/DataAccess/Secrets.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PrimeOrdersLibrary.DataAccess;
-
-public static class Secrets
-{
-	public static string DatabaseUserId => "aadisql";
-	public static string DatabaseName => "PrimeOrders";
-	public static string DatabasePassword => "Aadi1234";
-	public static string SyncfusionLicense => "Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXhednRSRmNfVEB0XEJWYEk=";
-}
