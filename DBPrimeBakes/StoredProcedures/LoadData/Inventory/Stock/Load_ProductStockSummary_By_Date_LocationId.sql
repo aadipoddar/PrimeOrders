b4e63a78d1cd48a10f7e417f9aeab1aa4d2472d3
Aadi Poddar <aadipoddarmail@gmail.com>
2025-11-10 10:35:02 +0530

    Optimize Queries

--- DBPrimeBakes/StoredProcedures/LoadData/Inventory/Stock/Load_ProductStockSummary_By_Date_LocationId.sql
+++ DBPrimeBakes/StoredProcedures/LoadData/Inventory/Stock/Load_ProductStockSummary_By_Date_LocationId.sql
@@ -4,174 +4,109 @@
 	@LocationId INT
 AS
 BEGIN
+	SET NOCOUNT ON;
+
+	-- Pre-calculate all stock aggregations in a single pass for each product
+	WITH StockAggregates AS (
+		SELECT 
+			ProductId,
+			-- Opening Stock: sum of all quantities before FromDate
+			SUM(CASE WHEN TransactionDate < @FromDate THEN Quantity ELSE 0 END) AS OpeningStock,
+			
+			-- Purchase Stock: sum of positive quantities in date range
+			SUM(CASE WHEN TransactionDate >= @FromDate AND TransactionDate <= @ToDate AND Quantity > 0 
+				THEN Quantity ELSE 0 END) AS PurchaseStock,
+			
+			-- Sale Stock: sum of negative quantities in date range
+			SUM(CASE WHEN TransactionDate >= @FromDate AND TransactionDate <= @ToDate AND Quantity < 0 
+				THEN Quantity ELSE 0 END) AS SaleStock,
+			
+			-- Monthly Stock: sum of all quantities in date range
+			SUM(CASE WHEN TransactionDate >= @FromDate AND TransactionDate <= @ToDate 
+				THEN Quantity ELSE 0 END) AS MonthlyStock,
+			
+			-- Closing Stock: sum of all quantities up to ToDate
+			SUM(CASE WHEN TransactionDate <= @ToDate THEN Quantity ELSE 0 END) AS ClosingStock
+		FROM [ProductStock] WITH (NOLOCK)
+		WHERE LocationId = @LocationId
+		GROUP BY ProductId
+	),
+	-- Calculate average and weighted average prices for sales in date range
+	PriceAggregates AS (
+		SELECT 
+			ProductId,
+			AVG(CASE WHEN Quantity < 0 AND NetRate IS NOT NULL THEN NetRate ELSE NULL END) AS AveragePrice
+		FROM [ProductStock] WITH (NOLOCK)
+		WHERE LocationId = @LocationId
+			AND TransactionDate >= @FromDate 
+			AND TransactionDate <= @ToDate
+		GROUP BY ProductId
+	),
+	-- Get last sale price and date for each product in date range
+	LastSaleInfo AS (
+		SELECT 
+			ProductId,
+			NetRate AS LastSalePrice,
+			ROW_NUMBER() OVER (PARTITION BY ProductId ORDER BY TransactionDate DESC, Id DESC) AS RowNum
+		FROM [ProductStock] WITH (NOLOCK)
+		WHERE LocationId = @LocationId
+			AND TransactionDate >= @FromDate 
+			AND TransactionDate <= @ToDate
+			AND Quantity < 0
+			AND NetRate IS NOT NULL
+	),
+	-- Get product location rate
+	ProductLocationRate AS (
+		SELECT 
+			ProductId,
+			Rate,
+			ROW_NUMBER() OVER (PARTITION BY ProductId ORDER BY Id DESC) AS RowNum
+		FROM dbo.ProductLocation WITH (NOLOCK)
+		WHERE LocationId = @LocationId
+			AND Status = 1
+	)
+	-- Final select combining all pre-calculated data
 	SELECT
-        s.ProductId,
-        p.[Name] AS ProductName,
-        p.Code AS ProductCode,
-        p.ProductCategoryId,
-        pc.[Name] ProductCategoryName,
-
-        ISNULL
-        (
-           (SELECT SUM (Quantity)
-            FROM [ProductStock]
-            WHERE     ProductId = s.ProductId
-                  AND TransactionDate < @FromDate
-                  AND LocationId = @LocationId),
-           0) AS OpeningStock,
-
-        ISNULL
-        (
-           (SELECT SUM (Quantity)
-            FROM [ProductStock]
-            WHERE     ProductId = s.ProductId
-                  AND TransactionDate >= @FromDate
-                  AND TransactionDate <= @ToDate
-                  AND Quantity > 0
-                  AND LocationId = @LocationId),
-           0) AS PurchaseStock,
-
-        ISNULL
-        (
-           (SELECT SUM (Quantity)
-            FROM [ProductStock]
-            WHERE     ProductId = s.ProductId
-                  AND TransactionDate >= @FromDate
-                  AND TransactionDate <= @ToDate
-                  AND Quantity < 0
-                  AND LocationId = @LocationId),
-           0) AS SaleStock,
-
-        ISNULL
-        (
-           (SELECT SUM (Quantity)
-            FROM [ProductStock]
-            WHERE     ProductId = s.ProductId
-                  AND TransactionDate >= @FromDate
-                  AND TransactionDate <= @ToDate
-                  AND LocationId = @LocationId),
-           0) AS MonthlyStock,
-
-        ISNULL
-        (
-           (SELECT SUM (Quantity)
-            FROM [ProductStock]
-            WHERE     ProductId = s.ProductId
-                  AND TransactionDate <= @ToDate
-                  AND LocationId = @LocationId),
-           0) AS ClosingStock,
-
-        ISNULL
-        (
-            (SELECT TOP 1 Rate
-                FROM dbo.ProductLocation
-                WHERE ProductId = s.ProductId
-                  AND LocationId = @LocationId
-                  AND Status = 1)
-         , p.Rate) AS Rate,
-
-        ISNULL
-        (
-            ISNULL
-            (
-                (SELECT TOP 1 Rate
-                    FROM dbo.ProductLocation
-                    WHERE ProductId = s.ProductId
-                      AND LocationId = @LocationId
-                      AND Status = 1)
-             , p.Rate) *
-                ISNULL
-                  (
-                     (SELECT SUM (Quantity)
-                      FROM [ProductStock]
-                      WHERE     ProductId = s.ProductId
-                            AND TransactionDate <= @ToDate
-                            AND LocationId = @LocationId),
-                     0)
-        , 0) AS ClosingValue,
-
-        ISNULL
-        (
-            (SELECT AVG (NetRate)
-             FROM [ProductStock]
-             WHERE     ProductId = s.ProductId
-                   AND TransactionDate >= @FromDate
-                   AND TransactionDate <= @ToDate
-                   AND Quantity < 0
-                   AND NetRate IS NOT NULL
-                   AND LocationId = @LocationId),
-        0) AS AveragePrice,
-
-        ISNULL
-        (
-            (SELECT AVG (NetRate) * 
-                (
-                    ISNULL
-                      (
-                         (SELECT SUM (Quantity)
-                          FROM [ProductStock]
-                          WHERE     ProductId = s.ProductId
-                                AND TransactionDate <= @ToDate
-                                AND LocationId = @LocationId),
-                         0)
-                )
-             FROM [ProductStock]
-             WHERE     ProductId = s.ProductId
-                   AND TransactionDate >= @FromDate
-                   AND TransactionDate <= @ToDate
-                   AND Quantity < 0
-                   AND NetRate IS NOT NULL
-                   AND LocationId = @LocationId),
-        0) AS WeightedAverageValue,
-
-        ISNULL
-        (
-            (SELECT TOP 1 NetRate
-             FROM [ProductStock]
-             WHERE     ProductId = s.ProductId
-                   AND TransactionDate >= @FromDate
-                   AND TransactionDate <= @ToDate
-                   AND Quantity < 0
-                   AND NetRate IS NOT NULL
-                   AND LocationId = @LocationId
-             ORDER BY TransactionDate DESC),
-        0) AS LastSalePrice,
-
-        ISNULL
-        (
-            (SELECT TOP 1 NetRate *
-                (
-                    ISNULL
-                      (
-                         (SELECT SUM (Quantity)
-                          FROM [ProductStock]
-                          WHERE     ProductId = s.ProductId
-                                AND TransactionDate <= @ToDate
-                                AND LocationId = @LocationId),
-                         0)
-                )
-             FROM [ProductStock]
-             WHERE     ProductId = s.ProductId
-                   AND TransactionDate >= @FromDate
-                   AND TransactionDate <= @ToDate
-                   AND Quantity < 0
-                   AND NetRate IS NOT NULL
-                   AND LocationId = @LocationId
-             ORDER BY TransactionDate DESC),
-        0) AS LastSaleValue
-
-    FROM
-        [ProductStock] s
-
-    LEFT JOIN
-        dbo.Product p ON p.Id = s.ProductId
-    LEFT JOIN
-        dbo.ProductCategory pc ON pc.Id = p.ProductCategoryId
-
-    GROUP BY s.ProductId,
-        p.[Name],
-        p.Code,
-        p.ProductCategoryId,
-        pc.Name,
-        p.Rate;
+		sa.ProductId,
+		p.[Name] AS ProductName,
+		p.Code AS ProductCode,
+		p.ProductCategoryId,
+		pc.[Name] ProductCategoryName,
+		
+		ISNULL(sa.OpeningStock, 0) AS OpeningStock,
+		ISNULL(sa.PurchaseStock, 0) AS PurchaseStock,
+		ISNULL(sa.SaleStock, 0) AS SaleStock,
+		ISNULL(sa.MonthlyStock, 0) AS MonthlyStock,
+		ISNULL(sa.ClosingStock, 0) AS ClosingStock,
+		
+		ISNULL(plr.Rate, p.Rate) AS Rate,
+		ISNULL(ISNULL(plr.Rate, p.Rate) * sa.ClosingStock, 0) AS ClosingValue,
+		
+		ISNULL(pa.AveragePrice, 0) AS AveragePrice,
+		ISNULL(pa.AveragePrice * sa.ClosingStock, 0) AS WeightedAverageValue,
+		
+		ISNULL(lsi.LastSalePrice, 0) AS LastSalePrice,
+		ISNULL(lsi.LastSalePrice * sa.ClosingStock, 0) AS LastSaleValue
+		
+	FROM StockAggregates sa
+	
+	LEFT JOIN dbo.Product p WITH (NOLOCK)
+		ON p.Id = sa.ProductId
+	
+	LEFT JOIN dbo.ProductCategory pc WITH (NOLOCK) 
+		ON pc.Id = p.ProductCategoryId
+		
+	LEFT JOIN PriceAggregates pa 
+		ON pa.ProductId = p.Id
+		
+	LEFT JOIN LastSaleInfo lsi 
+		ON lsi.ProductId = p.Id AND lsi.RowNum = 1
+		
+	LEFT JOIN ProductLocationRate plr 
+		ON plr.ProductId = sa.ProductId AND plr.RowNum = 1
+		
+	WHERE sa.OpeningStock != 0 
+		OR sa.PurchaseStock != 0 
+		OR sa.SaleStock != 0 
+		OR sa.ClosingStock != 0;
 END
--- DBPrimeBakes/StoredProcedures/LoadData/Inventory/Stock/Load_RawMaterialStockSummary_By_Date.sql
+++ DBPrimeBakes/StoredProcedures/LoadData/Inventory/Stock/Load_RawMaterialStockSummary_By_Date.sql
@@ -3,151 +3,94 @@
 	@ToDate DATE
 AS
 BEGIN
+	SET NOCOUNT ON;
+
+	-- Pre-calculate all stock aggregations in a single pass for each raw material
+	WITH StockAggregates AS (
+		SELECT 
+			RawMaterialId,
+			-- Opening Stock: sum of all quantities before FromDate
+			SUM(CASE WHEN TransactionDate < @FromDate THEN Quantity ELSE 0 END) AS OpeningStock,
+			
+			-- Purchase Stock: sum of positive quantities in date range
+			SUM(CASE WHEN TransactionDate >= @FromDate AND TransactionDate <= @ToDate AND Quantity > 0 
+				THEN Quantity ELSE 0 END) AS PurchaseStock,
+			
+			-- Sale Stock: sum of negative quantities in date range
+			SUM(CASE WHEN TransactionDate >= @FromDate AND TransactionDate <= @ToDate AND Quantity < 0 
+				THEN Quantity ELSE 0 END) AS SaleStock,
+			
+			-- Monthly Stock: sum of all quantities in date range
+			SUM(CASE WHEN TransactionDate >= @FromDate AND TransactionDate <= @ToDate 
+				THEN Quantity ELSE 0 END) AS MonthlyStock,
+			
+			-- Closing Stock: sum of all quantities up to ToDate
+			SUM(CASE WHEN TransactionDate <= @ToDate THEN Quantity ELSE 0 END) AS ClosingStock
+		FROM [RawMaterialStock] WITH (NOLOCK)
+		GROUP BY RawMaterialId
+	),
+	-- Calculate average prices for purchases in date range
+	PriceAggregates AS (
+		SELECT 
+			RawMaterialId,
+			AVG(CASE WHEN Quantity > 0 AND NetRate IS NOT NULL THEN NetRate ELSE NULL END) AS AveragePrice
+		FROM [RawMaterialStock] WITH (NOLOCK)
+		WHERE TransactionDate >= @FromDate 
+			AND TransactionDate <= @ToDate
+		GROUP BY RawMaterialId
+	),
+	-- Get last purchase price and date for each raw material in date range
+	LastPurchaseInfo AS (
+		SELECT 
+			RawMaterialId,
+			NetRate AS LastPurchasePrice,
+			ROW_NUMBER() OVER (PARTITION BY RawMaterialId ORDER BY TransactionDate DESC, Id DESC) AS RowNum
+		FROM [RawMaterialStock] WITH (NOLOCK)
+		WHERE TransactionDate >= @FromDate 
+			AND TransactionDate <= @ToDate
+			AND Quantity > 0
+			AND NetRate IS NOT NULL
+	)
+	-- Final select combining all pre-calculated data
 	SELECT
-        s.RawMaterialId,
-        r.[Name] RawMaterialName,
-        r.Code RawMaterialCode,
-        r.RawMaterialCategoryId,
-        rc.[Name] RawMaterialCategoryName,
-        r.UnitOfMeasurement,
-
-        ISNULL
-        (
-           (SELECT SUM (Quantity)
-            FROM [RawMaterialStock]
-            WHERE     RawMaterialId = s.RawMaterialId
-                  AND TransactionDate < @FromDate),
-           0) AS OpeningStock,
-
-        ISNULL
-        (
-           (SELECT SUM (Quantity)
-            FROM [RawMaterialStock]
-            WHERE     RawMaterialId = s.RawMaterialId
-                  AND TransactionDate >= @FromDate
-                  AND TransactionDate <= @ToDate
-                  AND Quantity > 0),
-           0) AS PurchaseStock,
-
-        ISNULL
-        (
-           (SELECT SUM (Quantity)
-            FROM [RawMaterialStock]
-            WHERE     RawMaterialId = s.RawMaterialId
-                  AND TransactionDate >= @FromDate
-                  AND TransactionDate <= @ToDate
-                  AND Quantity < 0),
-           0) AS SaleStock,
-
-        ISNULL
-        (
-           (SELECT SUM (Quantity)
-            FROM [RawMaterialStock]
-            WHERE     RawMaterialId = s.RawMaterialId
-                  AND TransactionDate >= @FromDate
-                  AND TransactionDate <= @ToDate),
-           0) AS MonthlyStock,
-
-        ISNULL
-        (
-           (SELECT SUM (Quantity)
-            FROM [RawMaterialStock]
-            WHERE     RawMaterialId = s.RawMaterialId
-                  AND TransactionDate <= @ToDate),
-           0) AS ClosingStock,
-
-        [r].Rate,
-
-        ISNULL
-        (
-            (SELECT [r].Rate *
-                (
-                    ISNULL
-                     (
-                        (SELECT SUM (Quantity)
-                         FROM [RawMaterialStock]
-                         WHERE     RawMaterialId = s.RawMaterialId
-                               AND TransactionDate <= @ToDate),
-                        0)
-                )
-             ), 0) AS ClosingValue,
-
-        ISNULL
-        (
-            (SELECT AVG (NetRate)
-             FROM [RawMaterialStock]
-             WHERE     RawMaterialId = s.RawMaterialId
-                   AND TransactionDate >= @FromDate
-                   AND TransactionDate <= @ToDate
-                   AND Quantity > 0
-                   AND NetRate IS NOT NULL),
-        0) AS AveragePrice,
-
-        ISNULL
-        (
-            (SELECT AVG (NetRate) * 
-                (
-                     ISNULL
-                     (
-                        (SELECT SUM (Quantity)
-                         FROM [RawMaterialStock]
-                         WHERE     RawMaterialId = s.RawMaterialId
-                               AND TransactionDate <= @ToDate),
-                        0)
-                )
-             FROM [RawMaterialStock]
-             WHERE     RawMaterialId = s.RawMaterialId
-                   AND TransactionDate >= @FromDate
-                   AND TransactionDate <= @ToDate
-                   AND Quantity > 0
-                   AND NetRate IS NOT NULL),
-        0) AS WeightedAverageValue,
-
-        ISNULL
-        (
-            (SELECT TOP 1 NetRate
-             FROM [RawMaterialStock]
-             WHERE     RawMaterialId = s.RawMaterialId
-                   AND TransactionDate >= @FromDate
-                   AND TransactionDate <= @ToDate
-                   AND Quantity > 0
-                   AND NetRate IS NOT NULL
-             ORDER BY TransactionDate DESC),
-        0) AS LastPurchasePrice,
-
-        ISNULL
-        (
-            (SELECT TOP 1 NetRate *
-                (
-                    ISNULL
-                     (
-                        (SELECT SUM (Quantity)
-                         FROM [RawMaterialStock]
-                         WHERE     RawMaterialId = s.RawMaterialId
-                               AND TransactionDate <= @ToDate),
-                        0)
-                )
-             FROM [RawMaterialStock]
-             WHERE     RawMaterialId = s.RawMaterialId
-                   AND TransactionDate >= @FromDate
-                   AND TransactionDate <= @ToDate
-                   AND Quantity > 0
-                   AND NetRate IS NOT NULL
-             ORDER BY TransactionDate DESC),
-        0) AS LastPurchaseValue
-    FROM
-        [RawMaterialStock] s
-
-    LEFT JOIN
-        dbo.RawMaterial r ON r.Id = s.RawMaterialId
-    LEFT JOIN
-        dbo.RawMaterialCategory rc ON rc.Id = r.RawMaterialCategoryId
-    
-    GROUP BY s.RawMaterialId,
-            r.[Name],
-            r.Code,
-            r.RawMaterialCategoryId,
-            rc.Name,
-            r.UnitOfMeasurement,
-            r.Rate;
+		sa.RawMaterialId,
+		r.[Name] RawMaterialName,
+		r.Code RawMaterialCode,
+		r.RawMaterialCategoryId,
+		rc.[Name] RawMaterialCategoryName,
+		r.UnitOfMeasurement,
+		
+		ISNULL(sa.OpeningStock, 0) AS OpeningStock,
+		ISNULL(sa.PurchaseStock, 0) AS PurchaseStock,
+		ISNULL(sa.SaleStock, 0) AS SaleStock,
+		ISNULL(sa.MonthlyStock, 0) AS MonthlyStock,
+		ISNULL(sa.ClosingStock, 0) AS ClosingStock,
+		
+		r.Rate,
+		ISNULL(r.Rate * sa.ClosingStock, 0) AS ClosingValue,
+		
+		ISNULL(pa.AveragePrice, 0) AS AveragePrice,
+		ISNULL(pa.AveragePrice * sa.ClosingStock, 0) AS WeightedAverageValue,
+		
+		ISNULL(lpi.LastPurchasePrice, 0) AS LastPurchasePrice,
+		ISNULL(lpi.LastPurchasePrice * sa.ClosingStock, 0) AS LastPurchaseValue
+		
+	FROM StockAggregates sa
+	
+	LEFT JOIN dbo.RawMaterial r WITH (NOLOCK)
+		ON r.Id = sa.RawMaterialId
+	
+	LEFT JOIN dbo.RawMaterialCategory rc WITH (NOLOCK) 
+		ON rc.Id = r.RawMaterialCategoryId
+		
+	LEFT JOIN PriceAggregates pa 
+		ON pa.RawMaterialId = sa.RawMaterialId
+		
+	LEFT JOIN LastPurchaseInfo lpi 
+		ON lpi.RawMaterialId = sa.RawMaterialId AND lpi.RowNum = 1
+		
+	WHERE sa.OpeningStock != 0 
+		OR sa.PurchaseStock != 0 
+		OR sa.SaleStock != 0 
+		OR sa.ClosingStock != 0;
 END
