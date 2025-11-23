61fd48d7e08ae1a9680aabf3eced662820cf83a5
Aadi Poddar <aadipoddarmail@gmail.com>
2025-11-23 20:20:42 +0530

    sales dashbaord

--- PrimeBakes/PrimeBakes.Shared/Pages/Sales/SalesDashboard.razor.cs
+++ PrimeBakes/PrimeBakes.Shared/Pages/Sales/SalesDashboard.razor.cs
@@ -4,67 +4,95 @@ using PrimeBakes.Shared.Services;
 
 using PrimeBakesLibrary.Models.Common;
 
-using Syncfusion.Blazor.Notifications;
-
 namespace PrimeBakes.Shared.Pages.Sales;
 
 public partial class SalesDashboard
 {
-	private UserModel _user;
 	private bool _isLoading = true;
-
-	private string _errorTitle = string.Empty;
-	private string _errorMessage = string.Empty;
-	private string _successTitle = string.Empty;
-	private string _successMessage = string.Empty;
-
-	private SfToast _sfErrorToast;
-	private SfToast _sfSuccessToast;
 
 	protected override async Task OnInitializedAsync()
 	{
 		_isLoading = true;
-		var authResult = await AuthService.ValidateUser(DataStorageService, NavigationManager, NotificationService, VibrationService);
-		_user = authResult.User;
+		await AuthService.ValidateUser(DataStorageService, NavigationManager, NotificationService, VibrationService, UserRoles.Sales, true);
 		_isLoading = false;
 	}
 
+	#region Sales Operations Navigation
 	private async Task NavigateToOrder()
 	{
-		if (!_user.Order)
-			await ShowToast("Access Denied", "You do not have permission to access the Order module. Please contact your administrator.", "error");
-
 		if (FormFactor.GetFormFactor() == "Web")
 			await JSRuntime.InvokeVoidAsync("open", PageRouteNames.Order, "_blank");
 		else
 			NavigationManager.NavigateTo(PageRouteNames.Order, true);
 	}
 
-	#region Utilities
-	private async Task ShowToast(string title, string message, string type)
+	private async Task NavigateToSale()
 	{
-		VibrationService.VibrateWithTime(200);
+		if (FormFactor.GetFormFactor() == "Web")
+			await JSRuntime.InvokeVoidAsync("open", PageRouteNames.Sale, "_blank");
+		else
+			NavigationManager.NavigateTo(PageRouteNames.Sale, true);
+	}
 
-		if (type == "error")
-		{
-			_errorTitle = title;
-			_errorMessage = message;
-			await _sfErrorToast.ShowAsync(new()
-			{
-				Title = _errorTitle,
-				Content = _errorMessage
-			});
-		}
-		else if (type == "success")
-		{
-			_successTitle = title;
-			_successMessage = message;
-			await _sfSuccessToast.ShowAsync(new()
-			{
-				Title = _successTitle,
-				Content = _successMessage
-			});
-		}
+	private async Task NavigateToSaleReturn()
+	{
+		if (FormFactor.GetFormFactor() == "Web")
+			await JSRuntime.InvokeVoidAsync("open", PageRouteNames.SaleReturn, "_blank");
+		else
+			NavigationManager.NavigateTo(PageRouteNames.SaleReturn, true);
 	}
 	#endregion
+
+	#region Sales Reports Navigation
+	private async Task NavigateToOrderReport()
+	{
+		if (FormFactor.GetFormFactor() == "Web")
+			await JSRuntime.InvokeVoidAsync("open", PageRouteNames.ReportOrder, "_blank");
+		else
+			NavigationManager.NavigateTo(PageRouteNames.ReportOrder, true);
+	}
+
+	private async Task NavigateToOrderItemReport()
+	{
+		if (FormFactor.GetFormFactor() == "Web")
+			await JSRuntime.InvokeVoidAsync("open", PageRouteNames.ReportOrderItem, "_blank");
+		else
+			NavigationManager.NavigateTo(PageRouteNames.ReportOrderItem, true);
+	}
+
+	private async Task NavigateToSaleReport()
+	{
+		if (FormFactor.GetFormFactor() == "Web")
+			await JSRuntime.InvokeVoidAsync("open", PageRouteNames.ReportSale, "_blank");
+		else
+			NavigationManager.NavigateTo(PageRouteNames.ReportSale, true);
+	}
+
+	private async Task NavigateToSaleItemReport()
+	{
+		if (FormFactor.GetFormFactor() == "Web")
+			await JSRuntime.InvokeVoidAsync("open", PageRouteNames.ReportSaleItem, "_blank");
+		else
+			NavigationManager.NavigateTo(PageRouteNames.ReportSaleItem, true);
+	}
+
+	private async Task NavigateToSaleReturnReport()
+	{
+		if (FormFactor.GetFormFactor() == "Web")
+			await JSRuntime.InvokeVoidAsync("open", PageRouteNames.ReportSaleReturn, "_blank");
+		else
+			NavigationManager.NavigateTo(PageRouteNames.ReportSaleReturn, true);
+	}
+
+	private async Task NavigateToSaleReturnItemReport()
+	{
+		if (FormFactor.GetFormFactor() == "Web")
+			await JSRuntime.InvokeVoidAsync("open", PageRouteNames.ReportSaleReturnItem, "_blank");
+		else
+			NavigationManager.NavigateTo(PageRouteNames.ReportSaleReturnItem, true);
+	}
+	#endregion
+
+	private async Task Logout() =>
+		await AuthService.Logout(DataStorageService, NavigationManager, NotificationService, VibrationService);
 }
