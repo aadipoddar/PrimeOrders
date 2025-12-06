58d0b13fd13f5e930d466fee6bff312b91447fc0
Aadi Poddar <aadipoddarmail@gmail.com>
2025-12-07 00:34:31 +0530

    Refactor Toggle Deleted Button Implementation

---
+++ PrimeBakes/PrimeBakes.Shared/Components/ToggleDeletedButton.razor.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components;
+
+namespace PrimeBakes.Shared.Components;
+
+public partial class ToggleDeletedButton
+{
+	[Parameter]
+	public bool Disabled { get; set; } = false;
+
+	[Parameter]
+	public bool ShowDeleted { get; set; } = false;
+
+	[Parameter]
+	public EventCallback OnToggle { get; set; }
+
+	private async Task HandleClick()
+	{
+		await OnToggle.InvokeAsync();
+	}
+}
