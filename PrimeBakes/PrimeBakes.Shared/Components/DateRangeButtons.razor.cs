1c5958bb85515f5244201f2af10e114fdfd4944f
Aadi Poddar <aadipoddarmail@gmail.com>
2025-12-06 20:07:17 +0530

    Refactor date range buttons into a reusable component

---
+++ PrimeBakes/PrimeBakes.Shared/Components/DateRangeButtons.razor.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components;
+
+using PrimeBakesLibrary.Models.Accounts.Masters;
+
+namespace PrimeBakes.Shared.Components;
+
+public partial class DateRangeButtons
+{
+	[Parameter]
+	public bool Disabled { get; set; } = false;
+
+	[Parameter]
+	public EventCallback<DateRangeType> OnDateRangeSelected { get; set; }
+}
